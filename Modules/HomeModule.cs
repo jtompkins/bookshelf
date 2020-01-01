fddd487f1ecab60d6525554a0dcb218a77d4c8de
Josh Tompkins <josh@joshtompkins.com>
2020-01-01 13:01:32 -0600

    Initial commit

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,8 @@
+using Carter;
+using Microsoft.AspNetCore.Http;
+
+public class HomeModule : CarterModule {
+  public HomeModule() {
+    Get("/", async (req, res) => await res.WriteAsync("hello, world!"));
+  }
+}
